6855f703fdfdcf99c5f38d7cb26397ceccfa8e02
Desiree Mendes <desireefrost@live.ca>
2021-09-25 16:17:43 -0400

    implemented function and passed tests

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,14 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    let verticalJoin = '';
+    for (let i = 0; i < letters[0].length; i++) {
+        for (let j = 0; j < letters.length; j++) {
+            verticalJoin = verticalJoin + letters[j][i];
+        }
+    }
+    if (verticalJoin.includes(word)) return true;
+    return false;
 }
 
 module.exports = wordSearch
