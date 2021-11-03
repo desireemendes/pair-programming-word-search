a5d9985c6118fffb6f1ece039cfbea1abf0a5a3c
Desiree Mendes <desireefrost@live.ca>
2021-11-03 16:30:14 -0400

    pair programming vertical function

--- wordsearch.js
+++ wordsearch.js
@@ -1,16 +1,25 @@
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-    let verticalJoin = '';
+    if (word === []) return false;
+
+    let returnValue = false
+    for (let l of horizontalJoin) {
+        
+        if (l.includes(word)) returnValue = true; 
+    } 
+    
+    let verticalJoin = ''
     for (let i = 0; i < letters[0].length; i++) {
         for (let j = 0; j < letters.length; j++) {
-            verticalJoin = verticalJoin + letters[j][i];
+         verticalJoin = verticalJoin + letters[j][i];
         }
+        if (verticalJoin.includes(word)) returnValue = true;
     }
-    if (verticalJoin.includes(word)) return true;
-    return false;
+
+    return returnValue;
+    
+
 }
 
 module.exports = wordSearch
